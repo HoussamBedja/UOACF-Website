9412af33b6793412530131f97fc356571d2aa808
Houssam <houssam.b@hotmail.com>
2016-09-28 13:43:54 -0400

    translated quote to french

--- eventmodalscript.js
+++ eventmodalscript.js
@@ -4,12 +4,29 @@ var modal = document.getElementById('myModal');
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
+
+
+// Get the modal
+var modal2 = document.getElementById('myModal2');
+// Get the other button that opens the modal MEETING
+var btn2 = document.getElementById("myBtn2");
+
+
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+// Get the <span> element that closes the modal
+var span2 = document.getElementsByClassName("close")[0];
+
 
 // When the user clicks on the button, open the modal 
 btn.onclick = function() {
     modal.style.display = "block";
+}
+
+
+// When the user clicks on the button, open the modal 
+btn2.onclick = function() {
+    modal2.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -17,9 +34,18 @@ span.onclick = function() {
     modal.style.display = "none";
 }
 
+// When the user clicks on <span> (x), close the modal
+span2.onclick = function() {
+    modal2.style.display = "none";
+}
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+    else if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
 }
+
